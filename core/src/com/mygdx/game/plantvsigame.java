ec9954d77886c6eb006abb1ec5bff61ca7a1afeb
m129907 <30289691+m129907@users.noreply.github.com>
2017-12-21 20:23:25 +0700

    still trying to get enemy working

--- core/src/com/mygdx/game/plantvsigame.java
+++ core/src/com/mygdx/game/plantvsigame.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Random;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
@@ -18,6 +19,10 @@ public class plantvsigame extends ApplicationAdapter {
 	Texture bulletT;
 	Texture enemyT;
 	
+	int amoutx = 30;
+	Random r = new Random();
+	
+		
 	float screenwidth;
 	float screenheight;
 	
@@ -36,29 +41,43 @@ public class plantvsigame extends ApplicationAdapter {
 		shooter= new Texture("shooter.png");
 		background = new Texture("frontyard.jpg");
 		bulletT = new Texture("bullet.png");
+		enemyT = new Texture("enemy.png");
 		shooterlocation = new Vector2(265,(screenheight/2)-(shooter.getHeight()/2));
 	}
 
-	public void update () {
+	public void update () throws InterruptedException {
 		if(Gdx.input.isKeyPressed(Keys.UP)) {
 			
-			shooterlocation.y = shooterlocation.y + 3;
+			shooterlocation.y = shooterlocation.y + 5;
 		}
 		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
 		
-			shooterlocation.y = shooterlocation.y - 3;
+			shooterlocation.y = shooterlocation.y - 5;
 		}
 		if(Gdx.input.isKeyJustPressed(Keys.SPACE)) {
 			Bullet myBullet = new Bullet(shooterlocation,new Vector2(4,0));
 			bulletManager.add(myBullet);
 		}
 		
+		if(Gdx.input.isKeyJustPressed(Keys.E)) {
+			
+		Enemy myEnemy = new Enemy(shooterlocation,new Vector2(-2,0));
+		enemyManager.add(myEnemy);
+		
+		 }
 		
 	}
 	
 	@Override
 	public void render () {
-		update();
+		int random = (int )(Math.random() * 200 );
+		try {
+			update();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -73,6 +92,14 @@ public class plantvsigame extends ApplicationAdapter {
 			currentBullet.Update();
 			batch.draw(bulletT,currentBullet.bulletlocation.x,currentBullet.bulletlocation.y);
 			count++;
+		}
+		int counter = 0;
+		while(counter < enemyManager.size())
+		{
+			Enemy currentEnemy = enemyManager.get(counter);
+			currentEnemy.Update();
+			batch.draw(enemyT,currentEnemy.enemylocation.x+1000,currentEnemy.enemylocation.y+random);
+			counter++;
 		}
 		
 		
