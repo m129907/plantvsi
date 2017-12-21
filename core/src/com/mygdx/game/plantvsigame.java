50533e61b70a4c8538c3cdaf3d158b656e3b33e2
m129907 <30289691+m129907@users.noreply.github.com>
2017-12-21 17:59:25 +0700

    shooter location.x/.y

--- core/src/com/mygdx/game/plantvsigame.java
+++ core/src/com/mygdx/game/plantvsigame.java
@@ -5,26 +5,42 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class plantvsigame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture shooter;
 	Texture background;
 	
+	float screenwidth;
+	float screenheight;
+	
+	Vector2 shooterlocation = new Vector2(0,0);
+	
 	@Override
 	public void create () {
+		screenwidth = Gdx.graphics.getWidth();
+		screenheight = Gdx.graphics.getHeight();
+		
 		batch = new SpriteBatch();
 		shooter= new Texture("shooter.png");
 		background = new Texture("frontyard.jpg");
+		
+		shooterlocation = new Vector2(265,(screenheight/2)-(shooter.getHeight()/2));
 	}
 
+	public void update () {
+		//if(Gdx.input.isKeyPressed(Keys.UP))
+	}
+	
 	@Override
 	public void render () {
+		update();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(background,0,0);
-		batch.draw(shooter, 0, 0);
+		batch.draw(shooter,shooterlocation.x, shooterlocation.y);
 		batch.end();
 	}
 	
