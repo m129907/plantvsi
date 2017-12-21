018f8b8b7a853337d71b891b2ff88fc810f2ebc6
m129907 <30289691+m129907@users.noreply.github.com>
2017-12-21 18:02:05 +0700

    Keyup go up keydown go down

--- core/src/com/mygdx/game/plantvsigame.java
+++ core/src/com/mygdx/game/plantvsigame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -30,7 +31,12 @@ public class plantvsigame extends ApplicationAdapter {
 	}
 
 	public void update () {
-		//if(Gdx.input.isKeyPressed(Keys.UP))
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+			shooterlocation.y = shooterlocation.y + 3;
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			shooterlocation.y = shooterlocation.y - 3;
+		}
 	}
 	
 	@Override
