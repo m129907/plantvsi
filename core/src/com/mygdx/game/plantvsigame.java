ebb608f42360c883b081342adc4956b8fad3dc7c
m129907 <30289691+m129907@users.noreply.github.com>
2017-12-21 17:31:12 +0700

    add shooter

--- core/src/com/mygdx/game/plantvsigame.java
+++ core/src/com/mygdx/game/plantvsigame.java
@@ -8,26 +8,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class plantvsigame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture shooter;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shooter= new Texture("shooter.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(shooter, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		shooter.dispose();
 	}
 }
